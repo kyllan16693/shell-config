dc2e3eea9d7cc83355ce4ae7bddd6f54102eb7ea
Kyllan Wunder <fencer4933@gmail.com>
2025-07-31 09:35:54 -0400

    removing p10k, replacing with ohmyposh

--- html/install_shell_tools.sh
+++ html/install_shell_tools.sh
@@ -27,186 +27,46 @@ if ! declare -f spinner &>/dev/null; then
     download_and_source_script "utils.sh"
 fi
 
-# Function to install zsh-autosuggestions
-install_zsh_autosuggestions() {
-    if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
-        log_info "zsh-autosuggestions is already installed."
+# Function to install oh-my-posh
+install_oh_my_posh() {
+    if command -v oh-my-posh &>/dev/null; then
+        log_info "oh-my-posh is already installed."
     else
-        git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" >"$temp_file" 2>&1 &
-        spinner $! "Installing zsh-autosuggestions"
+        curl -s https://ohmyposh.dev/install.sh | bash -s >"$temp_file" 2>&1 &
+        spinner $! "Installing oh-my-posh"
     fi
 }
 
-# Function to install zsh-syntax-highlighting
-install_zsh_syntax_highlighting() {
-    if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
-        log_info "zsh-syntax-highlighting is already installed."
-    else
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" >"$temp_file" 2>&1 &
-        spinner $! "Installing zsh-syntax-highlighting"
-    fi
-}
-
-#Function to install zsh-vi-mode
-install_zsh_vi_mode() {
-    if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-vi-mode" ]; then
-        log_info "zsh-vi-mode is already installed."
-    else
-        git clone https://github.com/jeffreytse/zsh-vi-mode.git $ZSH_CUSTOM/plugins/zsh-vi-mode >"$temp_file" 2>&1 &
-        spinner $! "Installing zsh-vi-mode"
-    fi
-}
-
-# Function to install Powerlevel10k
-install_powerlevel10k() {
-    if [ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
-        log_info "Powerlevel10k is already installed."
-    else
-        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" >"$temp_file" 2>&1 &
-        spinner $! "Installing Powerlevel10k"
-    fi
-    if ! grep -q 'ZSH_THEME="powerlevel10k/powerlevel10k"' "$HOME/.zshrc"; then
-        if [[ "$OSTYPE" == "darwin"* ]]; then
-            sed -i '' 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' "$HOME/.zshrc"
-        else
-            sed -i 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' "$HOME/.zshrc"
-        fi
-        log_info "Enabled Powerlevel10k theme for oh-my-zsh."
-    else
-        log_info "Powerlevel10k theme is already enabled."
-    fi
-
-    if [ -f "$HOME/.p10k.zsh" ]; then
-        ask "p10k.zsh is already installed. Do you want to overwrite it?" || return
-    fi
-
-    is_headless
-    if [ $? -eq 1 ]; then
-        curl -fsSL https://kassel.sh/conf/p10k-headless.zsh -o "$HOME/.p10k.zsh" >"$temp_file" 2>&1 &
-    else
-        curl -fsSL https://kassel.sh/conf/p10k.zsh -o "$HOME/.p10k.zsh" >"$temp_file" 2>&1 &
-    fi
-    spinner $! "Downloading p10k.zsh"
-
-    # Add configuration to .zshrc
-    echo '# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.' >>"$HOME/.zshrc"
-    echo '# Initialization code that may require console input (password prompts, [y/n]' >>"$HOME/.zshrc"
-    echo '# confirmations, etc.) must go above this block; everything else may go below.' >>"$HOME/.zshrc"
-    echo 'if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then' >>"$HOME/.zshrc"
-    echo '  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"' >>"$HOME/.zshrc"
-    echo 'fi' >>"$HOME/.zshrc"
-    echo '' >>"$HOME/.zshrc"
-    echo '# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.' >>"$HOME/.zshrc"
-    echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >>"$HOME/.zshrc"
-}
-
-# Function to install oh-my-zsh
-install_oh_my_zsh() {
-    if [ -d "$HOME/.oh-my-zsh" ]; then
-        log_info "oh-my-zsh is already installed."
-    else
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" >"$temp_file" 2>&1 &
-        spinner $! "Installing oh-my-zsh"
-    fi
-
-    install_zsh_autosuggestions
-    install_zsh_syntax_highlighting
-    install_zsh_vi_mode
-    install_powerlevel10k
+# Function to configure oh-my-posh
+configure_oh_my_posh() {
+    # Create .ohmyposh directory if it doesn't exist
+    mkdir -p "$HOME/.ohmyposh"
     
-    if ! grep -q "zsh-autosuggestions" "$HOME/.zshrc"; then
-        if [[ "$OSTYPE" == "darwin"* ]]; then
-            sed -i '' 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-vi-mode)/' "$HOME/.zshrc"
-        else
-            sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-vi-mode)/' "$HOME/.zshrc"
+    # Download the config.toml file
+    if [ -f "$HOME/.ohmyposh/config.toml" ]; then
+        if ask "oh-my-posh config.toml already exists. Do you want to overwrite it?"; then
+            curl -fsSL "$BASE_URL/conf/config.toml" -o "$HOME/.ohmyposh/config.toml" >"$temp_file" 2>&1 &
+            spinner $! "Downloading oh-my-posh config.toml"
         fi
-        
-        echo "" >>"$HOME/.zshrc"
-        echo "# Set bindkey to vi mode" >>"$HOME/.zshrc"
-        echo "bindkey -v" >>"$HOME/.zshrc"
-
-        log_info "Enabled git, zsh-autosuggestions, and zsh-syntax-highlighting plugins for oh-my-zsh."
     else
-        log_info "Plugins already configured in .zshrc."
+        curl -fsSL "$BASE_URL/conf/config.toml" -o "$HOME/.ohmyposh/config.toml" >"$temp_file" 2>&1 &
+        spinner $! "Downloading oh-my-posh config.toml"
     fi
-}
-
-# Function to install bash-completion
-install_bash_completion() {
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        if ! brew list bash-completion &>/dev/null; then
-            brew install bash-completion >"$temp_file" 2>&1 &
-            spinner $! "Installing bash-completion"
-        else
-            log_info "bash-completion is already installed."
+    
+    # Download the .zshrc file
+    if [ -f "$HOME/.zshrc" ]; then
+        if ask ".zshrc already exists. Do you want to back it up and overwrite it?"; then
+            cp "$HOME/.zshrc" "$HOME/.zshrc.bak"
+            log_info "Backup created at $HOME/.zshrc.bak"
+            curl -fsSL "$BASE_URL/conf/.zshrc" -o "$HOME/.zshrc" >"$temp_file" 2>&1 &
+            spinner $! "Downloading .zshrc"
         fi
-    elif ! dpkg -l | grep -qw bash-completion; then
-        sudo apt-get install -y bash-completion >"$temp_file" 2>&1 &
-        spinner $! "Installing bash-completion"
     else
-        log_info "bash-completion is already installed."
+        curl -fsSL "$BASE_URL/conf/.zshrc" -o "$HOME/.zshrc" >"$temp_file" 2>&1 &
+        spinner $! "Downloading .zshrc"
     fi
-}
-
-# Function to install grc using the automated script
-install_grc() {
-    if ! command -v grc &>/dev/null; then
-        bash -c "$(curl -fsSL https://github.com/garabik/grc/raw/master/grc.sh)" >"$temp_file" 2>&1 &
-        spinner $! "Installing grc"
-    else
-        log_info "grc is already installed."
-        return 0
-    fi
-
-    # Download grc.sh for alias definitions
-    curl -fsSL https://github.com/garabik/grc/raw/master/grc.sh -o "$HOME/.grc.sh" >"$temp_file" 2>&1 &
-    spinner $! "Downloading grc.sh"
-
-    if ! grep -q "source $HOME/.grc.sh" "$HOME/.bashrc"; then
-        echo 'export GRC_ALIASES=true' >>"$HOME/.bashrc"
-        echo "source $HOME/.grc.sh" >>"$HOME/.bashrc"
-    fi
-}
-
-# Function to configure Powerbash10k for Oh-My-Bash
-configure_powerbash10k() {
-    if ! grep -q 'OSH_THEME="powerbash10k/powerbash10k"' "$HOME/.bashrc"; then
-        log_info "Configuring Powerbash10k theme for Oh-My-Bash..."
-        if [[ "$OSTYPE" == "darwin"* ]]; then
-            sed -i '' 's|OSH_THEME=".*"|OSH_THEME="powerbash10k"|' "$HOME/.bashrc"
-        else
-            sed -i 's|OSH_THEME=".*"|OSH_THEME="powerbash10k"|' "$HOME/.bashrc"
-        fi
-        log_info "Enabled Powerbash10k theme for Oh-My-Bash."
-    else
-        log_info "Powerbash10k theme is already enabled."
-    fi
-}
-
-# Function to install oh-my-bash
-install_oh_my_bash() {
-    if [ -d "$HOME/.oh-my-bash" ]; then
-        log_info "oh-my-bash is already installed."
-    else
-        bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" >"$temp_file" 2>&1 &
-        spinner $! "Installing oh-my-bash"
-    fi
-
-    install_bash_completion
-    install_grc
-    configure_powerbash10k
-
-    if grep -q "source /usr/share/bash-completion/bash_completion" "$HOME/.bashrc"; then
-        if ask "Configuration for bash-completion already exists. Do you want to back it up?"; then
-            cp "$HOME/.bashrc" "$HOME/.bashrc.bak"
-            log_info "Backup created at $HOME/.bashrc.bak."
-        fi
-    fi
-
-    if ! grep -q "bash_completion" "$HOME/.bashrc"; then
-        echo 'source /usr/share/bash-completion/bash_completion' >>"$HOME/.bashrc"
-    fi
-    log_info "Enabled bash-completion and grc for oh-my-bash."
+    
+    log_info "oh-my-posh configuration completed."
 }
 
 # Function to install zsh
@@ -239,7 +99,8 @@ install_zsh() {
 install_shell_tools() {
     current_shell=$(basename "$SHELL")
     log_debug "Current shell: $current_shell"
-    # if the current shell is bash ask if the user wants to switch to zsh
+    
+    # If the current shell is bash, ask if the user wants to switch to zsh
     if [[ "$current_shell" == "bash" ]]; then
         if ask "Do you want to switch to zsh?"; then
             # Change the default shell to zsh
@@ -251,11 +112,11 @@ install_shell_tools() {
     fi
 
     if [[ "$current_shell" == "zsh" ]]; then
-        install_oh_my_zsh
-    elif [[ "$current_shell" == "bash" ]]; then
-        install_oh_my_bash
+        install_oh_my_posh
+        configure_oh_my_posh
+        log_info "Shell tools installation completed. Please restart your terminal or run 'source ~/.zshrc' to apply changes."
     else
-        log_error "Unsupported shell: $current_shell. Exiting..."
+        log_error "Unsupported shell: $current_shell. This script is designed for zsh. Exiting..."
         exit 1
     fi
 }
